3d0b804e4d11ac53d64a8017c1b8cdc1d1947c7c
yeef93 <yunindafaranika@gmail.com>
2024-03-25 15:31:31 +0700

    feat(session5-Exercise7): isanagram excecise

--- src/main/java/com/adepuu/exercises/session5/Exercise7.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise7.java
@@ -1,5 +1,9 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Exercise7 {
     /**
      * Java String Program to Check Anagram
@@ -13,6 +17,38 @@ public class Exercise7 {
      * Output: false
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("========Check Anagram========");
+        // Ask the user for fill the first text
+        System.out.println("Enter first text: ");
+        String first = scanner.next();
+        System.out.println("Enter second text: ");
+        String second = scanner.next();
+        System.out.println("First Text :"+first);
+        System.out.println("Second Text :"+second);
+        // Result
+        System.out.println("Result:"+isAnagram(first,second));
+    }
 
+    static boolean isAnagram(String first, String second){
+        boolean statusAnagram = true;
+        if(first.length() != second.length()){
+            statusAnagram = false;
+        }
+        else{
+            char[] characterString1 = first.toLowerCase().toCharArray();
+            char[] characterString2= second.toLowerCase().toCharArray();
+
+            // Sort strings
+            Arrays.sort(characterString1);
+            Arrays.sort(characterString2);
+
+            for(int i = 0; i < characterString2.length; i++){
+                if(characterString1[i] != characterString2[i]){
+                    statusAnagram = false;
+                }
+            }
+        }
+        return (statusAnagram);
     }
 }
